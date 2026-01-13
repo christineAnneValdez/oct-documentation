dc02312a73e9f53d549f96d51e4d82bfaf936829
christineAnneValdez <christineannevaldez2@gmail.com>
2026-01-13 21:13:45 +0800

    Edited Configuration

--- oct/docusaurus.config.ts
+++ oct/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://christineAnneValdez.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/oct-documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'christineAnneValdez', // Usually your GitHub org/user name.
+  projectName: 'oct-documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
