a9140422dac7399db2e0ff7efa16e7a80cfbea34
christineAnneValdez <christineannevaldez2@gmail.com>
2026-01-19 13:51:53 +0800

    edit dockerfile

--- oct/docusaurus.config.ts
+++ oct/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://christineAnneValdez.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/oct-documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
